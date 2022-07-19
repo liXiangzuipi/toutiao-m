7b783b8732e15a34b84b4675c2a2dc66129aea58
liXiangzuipi <992532683@qq.com>
2022-07-19 23:37:25 +0800

    vant移动端适配

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="div1"></div>
     <van-button type="primary">主要按钮</van-button>
     <i class="toutiao toutiao-dianzan"></i>
     <h1>APP</h1>
@@ -12,5 +13,9 @@ export default {
 }
 </script>
 <style lang="less">
-
+.div1{
+  height: 100px;
+  width: 100px;
+  background-color:red
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 import '@/styles/index.less'
 Vue.config.productionTip = false
 import { Button } from 'vant';
+//移动端适配第一步， npm i -S amfe-flexible 动态设置rem基准值  该解决方案为设置根字号为屏幕的十分之一
+import 'amfe-flexible'
+//移动适配第二步 npm i postscc-csstorem 官网步骤即可  将px转换为 rem 不包括行间样式
+
 
 Vue.use(Button);
 new Vue({
