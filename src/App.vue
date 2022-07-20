6ad425e3b2b594dac88c14ed7f898bf6c2009f2e
liXiangzuipi <992532683@qq.com>
2022-07-20 20:48:52 +0800

    登录页面路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div class="div1"></div>
-    <van-button type="primary">主要按钮</van-button>
-    <i class="toutiao toutiao-dianzan"></i>
-    <h1>APP</h1>
     <router-view/>
   </div>
 </template>
